f0f3f5bfa354f128501be7aa597170e18cdf2eec
andrewshort02-gif <andrewshort02@gmail.com>
2026-09-23 19:16:12 +0100

    Add files via upload
    Initial upload

---
+++ award-calculator-app/sw.js
@@ -0,0 +1,5 @@
+const CACHE="award-calc-v1";
+const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-180.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==CACHE).map(n=>caches.delete(n)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});

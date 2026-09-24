45f7ae4dac0cebdc5885e5b38ea1aed919530d47
andrewshort02-gif <andrewshort02@gmail.com>
2026-09-24 14:31:40 +0100

    v1.6
    Updated files added to bring up to v1.6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="award-calc-v1";
+const CACHE="award-calc-v5";
 const FILES=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-180.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(n=>n!==CACHE).map(n=>caches.delete(n)))).then(()=>self.clients.claim()))});
